7dd73237c9a0728b613a7e6fbaf7fa310551acbf
cranej <crane@cranejin.com>
2024-11-20 10:30:08 +0800

    Update style.css

--- config.lisp
+++ config.lisp
@@ -1,4 +1,4 @@
 (config site-title "cranej's personal site")
 (config site-base-url "https://cranejin.com")
-(config site-output-html-dir "public/www/")
+(config site-html-output-dir "public/www/")
 (be-verbose)
